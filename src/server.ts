b1c4da4bd84d10dc8022dade7c6ab10cb8e06002
Gautam <gautam@Gautams-MacBook-Air.local>
2025-11-03 23:31:07 +0530

    app listen update

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,10 @@ import dotenv from 'dotenv';
 
 
 dotenv.config();
-if (process.env.NODE_ENV !== 'production') {
-    const PORT = parseInt(process.env.PORT || '3000', 10);
-    const host = `0.0.0.0`;
 
-    app.listen(PORT, host, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
-}
+const PORT = parseInt(process.env.PORT || '3000', 10);
+const host = `0.0.0.0`;
+
+app.listen(PORT, host, () => {
+    console.log(`Server running on port ${PORT}`);
+});
