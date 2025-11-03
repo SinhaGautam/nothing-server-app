effcbe64389fbc7a54208c6f134b1cc0a730e99c
Gautam <gautam@Gautams-MacBook-Air.local>
2025-11-03 23:46:59 +0530

    vercel config fix

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,13 @@ import dotenv from 'dotenv';
 
 
 dotenv.config();
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = parseInt(process.env.PORT || '3000', 10);
+    const host = `0.0.0.0`;
 
-const PORT = parseInt(process.env.PORT || '3000', 10);
-const host = `0.0.0.0`;
+    app.listen(PORT, host, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
 
-app.listen(PORT, host, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+export default app;
